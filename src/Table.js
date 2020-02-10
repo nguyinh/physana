2581481eac7b944c7c442741fa7aaa7263adeb1d
nguyinh <florent.dupontnguyen@gmail.com>
2020-02-10 17:36:05 +0100

    Beautify & added mock data

--- src/Table.js
+++ src/Table.js
@@ -1,28 +1,51 @@
-import React, { useState } from 'react';
-import { Grid, AutoSizer, CellMeasurer, CellMeasurerCache } from 'react-virtualized';
-import styles from './Table.module.css';
+import React, { useState } from "react";
+import {
+  Grid,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache
+} from "react-virtualized";
+import styles from "./Table.module.css";
+
+
+const cache = new CellMeasurerCache({
+  defaultWidth: 150,
+  minWidth: 125,
+  fixedHeight: true
+});
+
 
 const InputCell = ({ initialValue, style, updateData }) => {
-  const [ value, setValue ] = useState(initialValue);
+  const [value, setValue] = useState(initialValue);
 
-  const onBlur = (e) => {
+  const onBlur = () => {
     updateData(value);
   };
 
-  const onChange = (e, f, g) => {
+  const onChange = e => {
     setValue(e.target.value);
   };
 
-  return <input
-    style={{height: style.height, width: style.width, background: style.background }}
-    value={value}
-    autoFocus
-    onBlur={onBlur}
-    onChange={onChange}/>
+  return (
+    <input
+      value={value}
+      className='cell-input'
+      autoFocus
+      onBlur={onBlur}
+      onChange={onChange}
+    />
+  );
 };
 
-const EditableCell = ({ children, style, updateData, isEditing, setIsEditing }) => {
-  const [ isEditable, setIsEditable ] = useState(false);
+
+const EditableCell = ({
+  children: value,
+  style,
+  updateData,
+  isEditing,
+  setIsEditing
+}) => {
+  const [isEditable, setIsEditable] = useState(false);
 
   const handleCellClick = () => {
     if (!isEditing) {
@@ -33,48 +56,48 @@ const EditableCell = ({ children, style, updateData, isEditing, setIsEditing }) 
 
   const handleDataUpdated = newValue => {
     setIsEditable(false);
-    setTimeout(() => setIsEditing(false), 100);
+    setTimeout(() => setIsEditing(false), 150);
     updateData(newValue);
   };
-
-  return <div
-    style={style}
-    className={styles.cell}
-    onClick={handleCellClick}>
-    {isEditable
-      ? <InputCell
-          style={{height: style.height, width: style.width, background: style.background }}
+  // console.log(style);
+  return (
+    <div
+      style={style}
+      className='cell-container'
+      onClick={handleCellClick}>
+      {isEditable ? (
+        <InputCell
+          style={style}
           updateData={handleDataUpdated}
-          initialValue={children}/>
-      : children
-    }
-  </div>
+          initialValue={value}
+          onClick={console.log(style)}
+        />
+      ) : (
+        <span className='cell-value'>{value}</span>
+      )}
+    </div>
+  );
 };
 
-const cache = new CellMeasurerCache({
-  defaultWidth: 100,
-  minWidth: 75,
-  fixedHeight: true
-});
 
 const Table = ({ data, updateData }) => {
   const gridData = data;
+  console.log(gridData);
+  const [isEditing, setIsEditing] = useState(false);
 
-  const [ isEditing, setIsEditing ] = useState(false);
-  
   const cellRenderer = ({ columnIndex, key, parent, rowIndex, style }) => {
     const cellStyle = {
       ...style,
-      backgroundColor: rowIndex % 2 && '#efefef',
-      textAlign: rowIndex === 0 && 'center'
+      backgroundColor: rowIndex % 2 && "#fafafc",
+      textAlign: rowIndex === 0 && "center"
     };
 
     const headerStyle = {
       ...style,
-      textAlign: rowIndex === 0 && 'center',
-      fontWeight: 'bold',
-      backgroundColor: '#dcdcdc'
-    }
+      textAlign: rowIndex === 0 && "center",
+      fontWeight: "bold",
+      backgroundColor: "#f3f4fb"
+    };
 
     return (
       <CellMeasurer
@@ -84,7 +107,7 @@ const Table = ({ data, updateData }) => {
         parent={parent}
         rowIndex={rowIndex}
       >
-        <EditableCell 
+        <EditableCell
           key={key}
           style={rowIndex === 0 ? headerStyle : cellStyle}
           isEditing={isEditing}
@@ -95,22 +118,26 @@ const Table = ({ data, updateData }) => {
         </EditableCell>
       </CellMeasurer>
     );
-  }
+  };
 
-  return <AutoSizer>
-    {({height, width}) => 
-      gridData.length !== 0 && <Grid
-        cellRenderer={cellRenderer}
-        columnCount={gridData[0].length}
-        columnWidth={cache.columnWidth}
-        deferredMeasurementCache={cache}
-        height={height}
-        rowCount={gridData.length}
-        rowHeight={30}
-        width={width}
-      />
-    }
-  </AutoSizer>;
+  return (
+    <AutoSizer>
+      {({ height, width }) =>
+        gridData.length !== 0 && (
+          <Grid
+            cellRenderer={cellRenderer}
+            columnCount={gridData[0].length}
+            columnWidth={cache.columnWidth}
+            deferredMeasurementCache={cache}
+            height={height}
+            rowCount={gridData.length}
+            rowHeight={40}
+            width={width}
+          />
+        )
+      }
+    </AutoSizer>
+  );
 };
 
 export default Table;
