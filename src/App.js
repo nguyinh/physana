e8f907af405bef4737f14fb5ad498cab95aae5cc
nguyinh <florent.dupontnguyen@gmail.com>
2020-02-10 09:32:38 +0100

    Read file from input

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import Table from './Table';
 import './App.css';
 
 function App() {
+  const handleFile = (e) => {
+    // console.log(e);
+    // console.log(e.target.files[0]);
+    // console.log(e.target.result);
+    const reader = new FileReader();
+    reader.onloadend = function (e) {
+      console.log(reader.result)
+      // console.log(e.target.result);
+    }
+    reader.readAsText(e.target.files[0]);
+    // console.log(data);dz
+  };
+
   return (
     <div className="App">
+      <input type='file' onChange={handleFile}/>
       <Table/>
     </div>
   );
